a19d765250a1d41d766c12479b4fb479a270e913
Yeung Yiu Hung <hkclex@gmail.com>
2017-04-06 21:52:59 +0800

    Inital Commit

---
+++ MastodonKit/MastodonKit.h
@@ -0,0 +1,19 @@
+//
+//  MastodonKit.h
+//  MastodonKit
+//
+//  Created by Yeung Yiu Hung on 6/4/2017.
+//  Copyright © 2017年 Yeung Yiu Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MastodonKit.
+FOUNDATION_EXPORT double MastodonKitVersionNumber;
+
+//! Project version string for MastodonKit.
+FOUNDATION_EXPORT const unsigned char MastodonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MastodonKit/PublicHeader.h>
+
+
